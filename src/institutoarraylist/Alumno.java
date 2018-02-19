73420db8b51d549271414664a64cb225dbe14504
DAW <DAW@10.2.1.34>
2018-02-19 20:13:23 +0100

    1

---
+++ src/institutoarraylist/Alumno.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package institutoarraylist;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Dni. Nombre; Fecha de nacimiento. Notas: Se almacena información del listado
+ * de notas de un alumno
+ */
+public class Alumno extends Persona {
+
+    private ArrayList<Nota> arrayNotas = new ArrayList<Nota>();
+
+    public Alumno(String dni, String nombre, Fecha fechaNac) {
+        super(dni, nombre, fechaNac);
+    }
+
+    public ArrayList<Nota> getArrayNotas() {
+        return this.arrayNotas;
+    }
+
+    public void setarrayNotas(ArrayList<Nota> notas) {
+        this.arrayNotas = notas;
+    }
+
+    public void addPupil(Nota moduloMasNota) {
+        boolean encontrado = false;
+        try {
+            if (moduloMasNota.getNota() < 0 || moduloMasNota.getNota() > 10) {
+                //Si la nota no es correcta se pone como -1
+                moduloMasNota.setNota((float) -1);
+                this.arrayNotas.add(moduloMasNota);
+                String nombre = moduloMasNota.getNombre().getNombreModulo();
+                Error error1 = new Error("\tNota menor que 1 o mayor que 10 en el modulo " + nombre);
+                throw error1;
+            } else {
+                try {
+                    Iterator<Nota> iterator = arrayNotas.iterator();
+                    Nota notas = null;
+
+                    int i = 0;
+                    while (iterator.hasNext() && (!(encontrado))) {
+                        notas = iterator.next();
+
+                        if ((notas.getNombre().getNombreModulo().equalsIgnoreCase(moduloMasNota.getNombre().getNombreModulo()))) {
+                            encontrado = true;
+                            String nombred = moduloMasNota.getNombre().getNombreModulo();
+                            Error error2 = new Error("\tEl modulo  " + nombred + " ya Existe");
+                            throw error2;
+                        }
+                    }
+                    if (!(encontrado)) {
+                        this.arrayNotas.add(moduloMasNota);
+                    }
+                } catch (Error error2) {
+                    System.out.printf(error2.getMessage());
+                }
+            }
+        } catch (Error error1) {
+
+            System.out.printf(error1.getMessage());
+        }
+    }
+
+//comprobacion por alumno: todos los módulos están aprobados?
+    public boolean todosModulosAprobados() {
+        boolean todos = true;
+
+        Iterator<Nota> it = this.arrayNotas.iterator();
+        Nota obj = null;
+        while (it.hasNext() && (todos)) {
+            obj = it.next();
+            if (obj.getNota() < 5) {
+                todos = false;
+            }
+        }
+        return todos;
+    }
+
+    //nota media por alumno
+    public float notaMedia() {
+        float med = 0;
+
+        for (Nota nota : this.arrayNotas) {
+            med += nota.getNota();
+        }
+        return med / this.arrayNotas.size();
+    }
+
+    //menor de edad
+    public String menorDeEdad() {
+        String texto = "";
+        texto += "\n" + super.toString();
+        if (super.getFechaNac().mayorDe(18)) {
+            texto += "| Es mayor de edad   |\n";
+        } else {
+            texto += "| Es menor de edad   |\n";
+        }
+        return texto;
+    }
+
+    public String toString() {
+         final String ROJO="\033[31;1m";
+        //linea de la tabla
+        String linea = "\t+" + hy(61, "-") + "+";
+
+        //linea separadora de alumnos
+        String texto = "";
+
+        //Es mayor de eadad?
+        texto += menorDeEdad();
+
+        //información de los módulos y el alumno
+        texto += linea + "\n\t| Modulo                 | Ciclo   | Profesor      | Nota     |\n" + linea;
+        //nota y nota media
+        float med = 0;
+
+        for (Nota nota : this.arrayNotas) {
+            //notas de cada módulo
+            texto += "\n\t" + nota.toString();
+
+            med += nota.getNota();
+        }
+        //cálculo de la nota media
+        float notaMedia = (float) med / this.arrayNotas.size();
+
+        //linea
+        String leftAlignFormat = "\t| %-20s     %-2.2f %-29s |";
+
+        texto += "\n\t+" + hy(61, "-") + "+";
+        //dar formato a un valor en medio de una concatenació
+        String textoAux = String.format(leftAlignFormat, "Nota media alumno: ", notaMedia, " ");
+        texto += "\n" + textoAux + "\n";
+        texto += "\t+" + hy(61, "-") + "+\n";
+        return texto;
+    }
+
+    //metodo estático para crear una linea de guiones
+    public static String hy(int cant, String caracter) {
+        String hyphen = "";
+        for (int i = 0; i < cant; i++) {
+            hyphen += caracter;
+        }
+        return hyphen;
+    }
+}
---
+++ src/institutoarraylist/Main.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package institutoarraylist;
+
+import static institutoarraylist.Alumno.hy;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DAW
+ */
+public class Main {
+
+    /**
+     * hora crea una clase ejecutable que realice lo siguiente:
+     *
+     *** Crea el ciclo daw con 2000 horas de clase. ** Crea todos los módulos
+     * de 1 daw (Crea los profesores que necesiten para los módulos). ** Crear 5
+     * alumnos para daw y matricularlo a los módulos creados. ** Después mostrar
+     * la información de todos los alumnos **o Información personal. **o
+     * Información de si mayor de edad. ** Modulo matriculado junto la nota y el
+     * profesor que lo imparte. ** Además habrá que mostrar la nota media del
+     * alumno.  Después se deberá mostrar que porcentaje de alumnos tienen
+     * todos los módulos aprobados y cuantos no.  También habrá que mostrar el
+     * nombre del alumno que mejor nota media tiene y el que peor
+     */
+    public static void main(String[] args) {
+
+        //profesores
+        Profesor profeProgra = new Profesor("12345d", "Victor", new Fecha(2, 2, 1980), "Ing. informatico", "victor@gmail.com");
+        Profesor profeEntorno = new Profesor("12346d", "Alex", new Fecha(1, 1, 1981), "Informatico", "alex@gmail.com");
+        Profesor profeDbase = new Profesor("12347d", "Jose", new Fecha(4, 4, 1974), "Matemático", "jose@gmail.com");
+        Profesor profeHtml = new Profesor("12348d", "David", new Fecha(5, 5, 1985), "Físico", "david@gmail.com");
+
+        //creación de los alumnos del ciclo
+        Alumno alumno1 = new Alumno("32341k", "Juanjo", new Fecha(14, 6, 2001));
+        Alumno alumno2 = new Alumno("32342k", "Sandra", new Fecha(1, 2, 2000));
+        Alumno alumno3 = new Alumno("32343k", "Miguel Angel", new Fecha(4, 8, 1992));
+        Alumno alumno4 = new Alumno("32344k", "David", new Fecha(1, 1, 1999));
+        Alumno alumno5 = new Alumno("32345k", "Sergio", new Fecha(4, 8, 1999));
+
+         //creación del ciclo
+        Ciclo daw = new Ciclo("daw", 2000);
+
+        //creación de los módulos
+        Modulo prog = new Modulo("Programacion", 8, daw, profeProgra);
+        Modulo ed = new Modulo("Entorno de Desarrollo", 3, daw, profeEntorno);
+        Modulo dbase = new Modulo("DBase", 3, daw, profeDbase);
+        Modulo html = new Modulo("HTML", 8, daw, profeHtml);
+
+        //agregar alumnos al ciclo
+        daw.getAlumno().add(alumno1);
+        daw.getAlumno().add(alumno2);
+        daw.getAlumno().add(alumno3);
+        daw.getAlumno().add(alumno4);
+        daw.getAlumno().add(alumno5);
+        
+        //Creación de notas de los alumnos según módulo
+        
+       // alumno1.addPupil(new Nota(prog, 6));
+        //Nota pup = new Nota(modulo1, 7);
+        alumno1.addPupil(new Nota(prog, 9));        
+        alumno1.addPupil(new Nota(ed, 7));
+        alumno1.addPupil(new Nota(dbase, 3));
+        alumno2.addPupil(new Nota(html, 8));
+        alumno2.addPupil(new Nota(ed, 6));
+        alumno2.addPupil(new Nota(prog, 8));
+        alumno2.addPupil(new Nota(dbase, 3));
+        alumno3.addPupil(new Nota(dbase, 7));
+        alumno3.addPupil(new Nota(html, 7));
+        alumno4.addPupil(new Nota(html, 8));
+        alumno5.addPupil(new Nota(html, 3));
+        alumno5.addPupil(new Nota(prog, 10));
+        alumno5.addPupil(new Nota(prog, 8));
+
+        
+        //impresión de toda la información del ciclo
+        System.out.println(daw.imprimirTodo());
+
+        //linea separadora
+        System.out.println("\t+" + hy(31,"+=") + "+");
+        
+        //porcentaje de alumnos aprobados en todo el ciclo        
+        System.out.printf("\tPorcentaje de aprobados en todo el ciclo %-2.1f%% \n\n",daw.porcentajeAprobadosCiclo());
+       
+        //Persona con nota más alta y más baja
+        System.out.println("\tPersona con Nota max: "+daw.personaNotaMaxima().getNombre());
+        System.out.println("\tPersona con Nota min: "+daw.personaNotaMinima().getNombre());
+        System.out.println("\n\n");
+    }
+
+}
